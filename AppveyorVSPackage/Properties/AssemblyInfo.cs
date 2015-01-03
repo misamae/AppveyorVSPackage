901dbf586186a2475ea86bf46ca7e4d6d4ef2b95
Meisam Emamjome <misamae@outlook.com>
2015-01-03 21:05:08 +0000

    Added AppveyorVSPackage.Test project

--- AppveyorVSPackage/Properties/AssemblyInfo.cs
+++ AppveyorVSPackage/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("AppveyorVSPackage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AppveyorVSPackage")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
-[assembly: ComVisible(false)]     
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("AppveyorVSPackage_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100171564b1dac3e00b17fe9490c402a733eed76b7075a89c9c01b74b3c2e9453217089ee4874b94b87e094e12f5716b7aa7a505dcd448476fe6ab0d5feaa367e2f5217d7ab285a083c68ee8bcfe655cab4386dc0f5a8100adcafc13b3b3096432c9320b47088f48ef8cde50015a59d0035d087a89b2a8a99fe67a34a63763470c4")]
